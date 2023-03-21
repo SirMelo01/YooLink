b248188197ae23cfe85d36e8dcb32d227e772f84
sr08678 <sebastian.rauch@stud.th-deg.de>
2023-03-21 15:23:16 +0100

    kajioz

--- config/settings/base.py
+++ config/settings/base.py
@@ -226,7 +226,7 @@ EMAIL_TIMEOUT = 5
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL.
-ADMIN_URL = "admin/"
+ADMIN_URL = "cmsadmin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = [("""Sebastian""", "yoolink@t-online.de")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
