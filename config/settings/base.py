a170482a740c1db675fe24560dcb8ee894cd0388
sr08678 <sebastian.rauch@stud.th-deg.de>
2023-04-06 11:06:06 +0200

    testdasd

--- config/settings/base.py
+++ config/settings/base.py
@@ -158,7 +158,7 @@ STATICFILES_FINDERS = [
 # MEDIA
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
-MEDIA_ROOT = str(APPS_DIR / "media")
+MEDIA_ROOT = str(APPS_DIR / "static" / "images" / "upload")
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = "/media/"
 
