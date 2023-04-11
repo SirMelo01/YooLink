71eede46eb63bddfc6cc238ee6b1d1a72beb896a
johannesroesner <roesnerj@icloud.com>
2023-04-11 12:23:53 +0200

    template1

--- yoolink/designtemplates/urls.py
+++ yoolink/designtemplates/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from yoolink.designtemplates.views import load_designtemp
+from django.views.generic import TemplateView
 
 app_name = "designtemplates"
 urlpatterns = [
     path("", view=load_designtemp, name="designtemplates"),
+    path("template1/", TemplateView.as_view(template_name="designs/template1.html"), name="template1"),
 ]
