56a4b3957d8a6aca5de464fa486455778d9a97e2
johannesroesner <roesnerj@icloud.com>
2023-04-14 16:39:03 +0200

    cookies menu

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("", include('django.contrib.auth.urls')),
     path("impressum/", TemplateView.as_view(template_name="pages/impressum.html"), name="impressum"),
     path("datenschutz/", TemplateView.as_view(template_name="pages/datenschutz.html"), name="datenschutz"),
+    path("cookies/", TemplateView.as_view(template_name="pages/cookies.html"), name="cookies"),
     path("cms/", include("yoolink.cms.urls", namespace="cms")),
     path("vorlagen/", include("yoolink.designtemplates.urls", namespace="designtemplates"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
