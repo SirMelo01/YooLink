076cec61802649860d1fd9f7598f53d9f4dc487a
sr08678 <sebastian.rauch@stud.th-deg.de>
2023-04-23 16:30:12 +0200

    fhdt

--- yoolink/cms/models.py
+++ yoolink/cms/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import os
 from PIL import Image
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 
 ## Produktiv und funktioniert
@@ -38,9 +39,11 @@ class fileentry(models.Model):
         super(fileentry, self).delete(*args, **kwargs)
 
 
-#def image_compressor(sender, **kwargs): 
-#    if kwargs["created"]:
-#        with Image.open(kwargs["instance"].file.path) as file:
-#            file.save(kwargs["instance"].file.path, optimize=True, quality=10)
-
-#post_save.connect(image_compressor, sender=fileentry)
+#class Blog(models.Model):
+#    title = models.CharField(max_length=255)
+#    date = models.DateField()
+#    author = models.ForeignKey(User, on_delete=models.CASCADE)
+#    body = models.TextField()
+    
+#    def __str__(self):
+#        return self.title + ' | ' + self.author
