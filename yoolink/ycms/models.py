c3605148a11f7d28a5669c2a957a32c6ff6921ef
Lennert Meißner <lennert.meissner@nordakademie.de>
2024-01-20 11:46:26 +0100

    Add Product Model and Product CMS Pages Main Design

--- yoolink/ycms/models.py
+++ yoolink/ycms/models.py
@@ -91,6 +91,10 @@ class Galerie(models.Model):
     def __str__(self):
         return self.title
 
+"""
+Blog
+"""
+
 def upload_to_blog_image(instance, filename):
     return f"yoolink/blogs/{instance.id}/{filename}"
 def default_code():
@@ -121,6 +125,110 @@ class Blog(models.Model):
         # Call the parent class's save method to actually save the model
         super(Blog, self).save(*args, **kwargs)
     
+
+""" 
+Products
+"""
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    website = models.CharField(max_length=200) 
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        # Slugify the title and store it in the slug field
+        self.slug = slugify(self.name)
+
+        # Call the parent class's save method to actually save the model
+        super(Brand, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+def upload_to_product_image(instance, filename):
+    return f"yoolink/products/{instance.id}/{filename}"
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    title_image = models.ImageField(upload_to=upload_to_product_image, default="", blank=True)
+    is_reduced = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_in_stock = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category, related_name='products')
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        # Slugify the title and store it in the slug field
+        self.slug = slugify(self.title)
+
+        # Call the parent class's save method to actually save the model
+        super(Product, self).save(*args, **kwargs)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    is_discounted = models.BooleanField(default=False)  # Flag to indicate if it's a discounted item
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
+    def get_price(self):
+        if self.is_discounted and self.discounted_price:
+            return self.discounted_price
+        return self.unit_price
+
+    def subtotal(self):
+        return self.quantity * self.get_price()
+
+    def __str__(self):
+        return f"{self.product.title} - {self.quantity} units - {'Discounted' if self.is_discounted else 'Normal'}"
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('OPEN', 'Offen'),
+        ('PAID', 'Bezahlt'),
+        ('COMPLETED', 'Abgeschlossen'),
+    ]
+
+    items = models.ManyToManyField(OrderItem)
+    buyer_email = models.EmailField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='OPEN')
+
+    def total(self):
+        return sum(item.subtotal() for item in self.items.all())
+
+    def __str__(self):
+        return f"Order - {self.buyer_email} - {self.status}"
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    comment = models.TextField()
+    rating = models.PositiveIntegerField(default=5)  # Assuming a rating out of 5
+
+    def __str__(self):
+        return f"{self.user_name} - {self.product.title} - {self.rating} stars"
+
+
+"""
+Messages
+"""
 
 class Message(models.Model):
     name = models.CharField(max_length=50)
