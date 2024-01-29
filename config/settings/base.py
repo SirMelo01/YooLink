9f249eca2cd6a38b6696b1d3ac815f0bbcbd47ae
sr08678 <sebastian.rauch@stud.th-deg.de>
2024-01-29 13:49:31 +0100

    mit default

--- config/settings/base.py
+++ config/settings/base.py
@@ -245,9 +245,9 @@ EMAIL_BACKEND = env(
     "DJANGO_EMAIL_BACKEND",
     default="django.core.mail.backends.smtp.EmailBackend",
 )
-EMAIL_HOST = env("DJANGO_EMAIL_HOST")
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = env("DJANGO_EMAIL_HOST", default="")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
 EMAIL_PORT = env("EMAIL_PORT", default=465)
 # EMAIL_USE_TLS = True
 EMAIL_USE_SSL = True
