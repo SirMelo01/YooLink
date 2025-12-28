f55d68bc70c04d64b4a845d30dda10cd55f67933
LennertMeiner-I22 <lennert.meissner@nordakademie.de>
2025-12-28 12:23:16 +0100

    Tried Unknown Problem Fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["yoolink.de"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
-DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
+DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=0)  # noqa F405
 
 # CACHES
 # ------------------------------------------------------------------------------
